5fc6973dca0d1bbd921afac5e0c2ad7d189e18b9
Epulapp <epulapp@epulapp1913.univ-lyon1.fr>
2020-02-23 18:14:32 +0100

    first commit

---
+++ CryptageTP/Encryption.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CryptageTP
+{
+    partial class Encryption
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input_TextBox = new System.Windows.Forms.TextBox();
+            this.Output_TextBox = new System.Windows.Forms.TextBox();
+            this.Key_TextBox = new System.Windows.Forms.TextBox();
+            this.Encrypt_Button = new System.Windows.Forms.Button();
+            this.Decrypt_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Input_TextBox
+            // 
+            this.Input_TextBox.Location = new System.Drawing.Point(12, 15);
+            this.Input_TextBox.Multiline = true;
+            this.Input_TextBox.Name = "Input_TextBox";
+            this.Input_TextBox.Size = new System.Drawing.Size(776, 169);
+            this.Input_TextBox.TabIndex = 0;
+            // 
+            // Output_TextBox
+            // 
+            this.Output_TextBox.Location = new System.Drawing.Point(13, 216);
+            this.Output_TextBox.Multiline = true;
+            this.Output_TextBox.Name = "Output_TextBox";
+            this.Output_TextBox.Size = new System.Drawing.Size(775, 191);
+            this.Output_TextBox.TabIndex = 1;
+            // 
+            // Key_TextBox
+            // 
+            this.Key_TextBox.Location = new System.Drawing.Point(13, 190);
+            this.Key_TextBox.Name = "Key_TextBox";
+            this.Key_TextBox.Size = new System.Drawing.Size(237, 20);
+            this.Key_TextBox.TabIndex = 2;
+            // 
+            // Encrypt_Button
+            // 
+            this.Encrypt_Button.Location = new System.Drawing.Point(266, 186);
+            this.Encrypt_Button.Name = "Encrypt_Button";
+            this.Encrypt_Button.Size = new System.Drawing.Size(99, 29);
+            this.Encrypt_Button.TabIndex = 3;
+            this.Encrypt_Button.Text = "Encrypt";
+            this.Encrypt_Button.UseVisualStyleBackColor = true;
+            this.Encrypt_Button.Click += new System.EventHandler(this.Encrypt_Button_Click);
+            // 
+            // Decrypt_Button
+            // 
+            this.Decrypt_Button.Location = new System.Drawing.Point(371, 186);
+            this.Decrypt_Button.Name = "Decrypt_Button";
+            this.Decrypt_Button.Size = new System.Drawing.Size(98, 29);
+            this.Decrypt_Button.TabIndex = 4;
+            this.Decrypt_Button.Text = "Decrypt";
+            this.Decrypt_Button.UseVisualStyleBackColor = true;
+            this.Decrypt_Button.Click += new System.EventHandler(this.Decrypt_Button_Click);
+            // 
+            // Encryption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 414);
+            this.Controls.Add(this.Decrypt_Button);
+            this.Controls.Add(this.Encrypt_Button);
+            this.Controls.Add(this.Key_TextBox);
+            this.Controls.Add(this.Output_TextBox);
+            this.Controls.Add(this.Input_TextBox);
+            this.Name = "Encryption";
+            this.Text = "Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input_TextBox;
+        private System.Windows.Forms.TextBox Output_TextBox;
+        private System.Windows.Forms.TextBox Key_TextBox;
+        private System.Windows.Forms.Button Encrypt_Button;
+        private System.Windows.Forms.Button Decrypt_Button;
+    }
+}
+
---
+++ CryptageTP/Encryption.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptageTP
+{
+    public partial class Encryption : Form
+    {
+        public Encryption()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt_Button_Click(object sender, EventArgs e)
+        {
+            Encrypt(Key_TextBox.Text, Input_TextBox.Text);
+        }
+        private void Decrypt_Button_Click(object sender, EventArgs e)
+        {
+            Decrypt(Key_TextBox.Text, Output_TextBox.Text);
+        }
+
+        private struct Pair
+        {
+            public char character;
+            public int position;
+        }
+
+        private void Encrypt(string key, string message)
+        {
+            // On détermine les positions des lettres de la clé
+            int[] order = new int[key.Length];
+
+            Pair[] sortedArray = new Pair[key.Length];
+            for (int i = 0; i < key.Length; i++)
+            {
+                sortedArray[i] = new Pair();
+                sortedArray[i].character = key[i];
+                sortedArray[i].position = i;
+            }
+            sortedArray = sortedArray.OrderBy(item => item.character).ToArray();
+
+            for (int i = 0; i < key.Length; i++)
+                order[i] = sortedArray[i].position;
+
+            // On remplit le tableau avec le message
+            string[] tab = new string[key.Length];
+            for (int i = 0; i < message.Length; i++)
+                tab[i % key.Length] += message[i];
+
+            // On lit le tableau colonne par colonne
+            Output_TextBox.Text = "";
+            foreach (int index in order)
+                Output_TextBox.Text += tab[index];
+        }
+        private void Decrypt(string key, string message)
+        {
+            // On détermine les positions des lettres de la clé
+            int[] order = new int[key.Length];
+
+            Pair[] sortedArray = new Pair[key.Length];
+            for (int i = 0; i < key.Length; i++)
+            {
+                sortedArray[i] = new Pair();
+                sortedArray[i].character = key[i];
+                sortedArray[i].position = i;
+            }
+            sortedArray = sortedArray.OrderBy(item => item.character).ToArray();
+
+            for (int i = 0; i < key.Length; i++)
+                order[i] = sortedArray[i].position;
+
+            // On remplit le tableau
+            int lineCount = message.Length / key.Length;
+
+            char[] tab = new char[lineCount * key.Length];
+            foreach (int index in order)
+            {
+
+            }
+        }
+    }
+}
